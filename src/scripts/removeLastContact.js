322c348d49c3c5f109bc01b6e59398b5f5d9928a
Artem-Oganezov <artem_oga@i.ua>
2025-06-04 22:42:56 +0300

    add file

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -3,6 +3,10 @@ import { writeContacts } from '../utils/writeContacts.js';
 
 export const removeLastContact = async () => {
   const contacts = await readContacts();
+  if (!contacts.length) {
+    console.log('No contacts');
+    return;
+  }
   contacts.pop();
   await writeContacts(contacts);
 };
